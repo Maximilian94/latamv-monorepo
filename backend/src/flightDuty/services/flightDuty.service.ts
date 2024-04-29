fc0fb01a0736d990645bf3cd1af6cc1890e66e56
Maximilian Kaden <mkadenfilgo@gmail.com>
2024-04-29 13:12:58 -0300

    feat: flight duty generation

--- backend/src/flightDuty/services/flightDuty.service.ts
+++ backend/src/flightDuty/services/flightDuty.service.ts
@@ -1,16 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { includes, sample } from 'lodash';
+import { includes, last, sample } from 'lodash';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { FlightSegment as FlightSegmentClass } from '../model/flightSegment';
 
 type FilterCriteria = {
-  airportsRoutes?: Array<string>;
-  filterDestinations?: Array<string>;
-};
-
-type FindPossibleRoutesV2Props = {
-  airportsConnections: AirportsConnection;
-  filterCriteria: FilterCriteria;
+  excludeAirports?: string[];
+  onlyDestinations?: string[];
 };
 
 type AirportConnectionData = {
@@ -25,22 +20,9 @@ export type GenerateFlightDutyParams = {
   doNotRepeatAirport?: boolean;
 };
 
-type FlightSegment = {
-  departure: string;
-  arrival: string;
-  numberOfFlights: number;
-  route: Array<string>;
-};
-
 @Injectable()
 export class FlightDutyService {
   constructor(private prisma: PrismaService) {}
-
-  finalRoute = [];
-
-  resetFinalRoute({ HUB }: { HUB: string }) {
-    this.finalRoute = [HUB];
-  }
 
   private createRouteInSegments = (
     numberOfTotalFlights: number,
@@ -75,97 +57,21 @@ export class FlightDutyService {
     return segments;
   };
 
-  private initSegments = (numberOfFlights: number) => {
-    const segments: FlightSegment[] = [];
-    let numberOfFlightsWile = numberOfFlights;
-    if (numberOfFlights > 3) {
-      while (numberOfFlightsWile > 0) {
-        let flightsInSegment;
-
-        if (numberOfFlightsWile % 4 == 0 || numberOfFlightsWile === 2) {
-          flightsInSegment = Math.min(4, numberOfFlightsWile);
-        } else {
-          flightsInSegment = Math.min(3, numberOfFlightsWile);
-        }
-
-        segments.push({
-          numberOfFlights: flightsInSegment,
-          departure: '',
-          arrival: null,
-          route: null,
-        });
-
-        numberOfFlightsWile -= flightsInSegment;
-      }
-    } else {
-      segments.push({
-        numberOfFlights,
-        departure: '',
-        arrival: null,
-        route: null,
-      });
-    }
-    return segments;
-  };
-
   async generateFlightDuty(params?: GenerateFlightDutyParams) {
     console.log('Inicia aqui');
-    const { numberOfFlights = 2, doNotRepeatAirport } = params;
+    const { numberOfFlights = 2 } = params;
     const HUB = 'SBGR';
-
-    this.resetFinalRoute({ HUB });
 
     // const airportsConnections = await this.getAirportConnectionsGraph();
 
     //  Will slipt the flightDuty in segments
-    const segments = this.initSegments(numberOfFlights);
     const segments2 = this.createRouteInSegments(numberOfFlights, HUB);
 
     await this.buildRoutes(segments2, HUB);
 
-    // console.log('Segmentos', segments);
-    console.log('segments2', segments2);
-    console.log('Rotas', segments2.map((segment) => segment.route).join(', '));
+    const flightDuty = segments2.map((segment) => segment.route);
 
-    // const addRoutes = async ({ index }: { index: number }) => {
-    //   const currentSegment = segments[index];
-    //   let nextSegment: FlightSegment = null;
-    //   if (+index == 0) currentSegment.departure = HUB;
-
-    //   if (+index == segments.length - 1) {
-    //     currentSegment.arrival = HUB;
-    //   } else {
-    //     nextSegment = segments[index + 1];
-    //   }
-
-    //   const possibleRoutes = await this.generatePossibleRoutes({
-    //     airportsConnections,
-    //     numberOfFlights: currentSegment.numberOfFlights,
-    //     startAirport: currentSegment.departure,
-    //     endAirport: currentSegment.arrival,
-    //     doNotRepeatAirport,
-    //   });
-
-    //   if (!possibleRoutes || possibleRoutes.length == 0) {
-    //     this.resetFinalRoute({ HUB });
-    //     return addRoutes({ index: 0 });
-    //   }
-
-    //   const route = sample(possibleRoutes);
-    //   this.finalRoute.push(...route);
-    //   currentSegment.arrival = route[route.length - 1];
-    //   currentSegment.route = route;
-    //   if (nextSegment) {
-    //     nextSegment.departure = route[route.length - 1];
-    //     addRoutes({ index: index + 1 });
-    //   }
-    // };
-
-    // addRoutes({ index: 0 });
-
-    return {
-      final: this.finalRoute,
-    };
+    return { flightDuty };
   }
 
   async getAirportConnectionsGraph() {
@@ -200,181 +106,105 @@ export class FlightDutyService {
     return airportsConnections;
   }
 
-  generatePossibleRoutes({
-    startAirport,
-    endAirport,
-    airportsConnections,
-    numberOfFlights,
-    doNotRepeatAirport,
-    airportsToAvoid,
+  generatePossibleRoutesFromAirport({
+    departureICAO,
+    airportConnectionData,
+    previousRoute,
+    filterCriteria,
   }: {
-    startAirport: string;
-    endAirport?: string;
-    airportsConnections: AirportsConnection;
-    numberOfFlights: number;
-    doNotRepeatAirport?: boolean;
-    airportsToAvoid?: Array<string>;
+    departureICAO: string;
+    airportConnectionData: AirportConnectionData;
+    previousRoute: string;
+    filterCriteria?: FilterCriteria;
   }) {
-    const possibleRoutes: Array<Array<string>> = [];
+    const possibleRoutes: string[] = [];
 
-    const addPossibleRoute = (possibleListRoute: Array<string>) => {
-      possibleRoutes.push(possibleListRoute.slice(1));
-    };
-
-    const findPossibility = ({
-      level,
-      startAirport,
-      list,
-      endAirport,
-    }: {
-      level: number;
-      startAirport: string;
-      list: Array<string>;
-      endAirport?: string;
-    }) => {
-      const isLastFlight = level == numberOfFlights;
-      if (isLastFlight) {
-        if (endAirport) {
-          if (endAirport == startAirport) addPossibleRoute(list);
-        } else {
-          addPossibleRoute(list);
-        }
-      } else {
-        for (const arp of airportsConnections[startAirport].destinations) {
-          if (doNotRepeatAirport) {
-            if (airportsToAvoid?.includes(arp)) {
-              return;
-            }
-
-            if (this.finalRoute.includes(arp)) {
-              if (arp == 'SBGR') {
-              } else {
-                return;
-              }
-            }
-          }
-
-          findPossibility({
-            level: level + 1,
-            startAirport: arp,
-            list: [...list, arp],
-            endAirport,
-          });
-        }
-      }
-    };
-
-    findPossibility({
-      startAirport,
-      level: 0,
-      list: [startAirport],
-      endAirport,
+    this.filterDestinations(
+      filterCriteria,
+      airportConnectionData?.destinations,
+    ).forEach((destination) => {
+      if (previousRoute)
+        possibleRoutes.push(`${previousRoute} - ${destination}`);
+      else possibleRoutes.push(`${departureICAO} - ${destination}`);
     });
 
     return possibleRoutes;
   }
 
-  filterAirports(criteria: FilterCriteria, connections: AirportsConnection) {
-    const filteredConnections: AirportsConnection = {};
-    const filterBy = (icao) => {
-      return includes(criteria.airportsRoutes, icao);
+  filterDestinations(filterCriteria: FilterCriteria, destinations: string[]) {
+    const excludeAirports = () => {
+      filteredDestinations = filteredDestinations.filter((destination) => {
+        return !filterCriteria?.excludeAirports.includes(destination);
+      });
     };
 
-    const filterDestinations = (destinations: Array<string>) => {
-      return destinations.filter((destination) =>
-        criteria.filterDestinations.includes(destination),
-      );
+    const onlyDestinations = () => {
+      filteredDestinations = filteredDestinations.filter((destination) => {
+        return filterCriteria?.onlyDestinations.includes(destination);
+      });
     };
 
-    // Iterar sobre cada entrada no dicionário
-    Object.entries(connections).forEach(([icao, data]) => {
-      const matchesAirportsRoutes = criteria.airportsRoutes
-        ? filterBy(icao)
-        : true;
+    let filteredDestinations = destinations;
+    filterCriteria?.excludeAirports ? excludeAirports() : true;
+    filterCriteria?.onlyDestinations ? onlyDestinations() : true;
 
-      // Se todos os critérios ativos são verdadeiros, adicionar ao resultado
-      if (matchesAirportsRoutes) {
-        filteredConnections[icao] = data;
-
-        if (criteria.filterDestinations) {
-          filteredConnections[icao].destinations = filterDestinations(
-            filteredConnections[icao].destinations,
-          );
-        }
-      }
-    });
-
-    console.log('filteredConnections', filteredConnections);
-
-    return filteredConnections;
+    return filteredDestinations;
   }
-
-  findPossibleRoutesV2 = ({
-    airportsConnections,
-    filterCriteria,
-  }: FindPossibleRoutesV2Props) => {
-    // console.log('airportsConnections', airportsConnections);
-    const filteredRoutes = this.filterAirports(
-      filterCriteria,
-      airportsConnections,
-    );
-    return filteredRoutes;
-  };
 
   async buildRoutes(flightSegments: FlightSegmentClass[], HUB: string) {
     const airportsConnections = await this.getAirportConnectionsGraph();
 
-    const addRoutes = async ({ index }: { index: number }) => {
-      console.log('Inicia addRoutes');
-      const currentSegment = flightSegments[index];
+    const addRoutes = async ({ segmentIndex }: { segmentIndex: number }) => {
+      const currentSegment = flightSegments[segmentIndex];
       let nextSegment: FlightSegmentClass = null;
-      if (+index == 0) currentSegment.departure = HUB;
+      if (+segmentIndex == 0) currentSegment.departure = HUB;
 
-      if (+index == flightSegments.length - 1) {
+      if (+segmentIndex == flightSegments.length - 1) {
         currentSegment.arrival = HUB;
       } else {
-        nextSegment = flightSegments[index + 1];
+        nextSegment = flightSegments[segmentIndex + 1];
       }
 
-      // const possibleRoutes = await this.generatePossibleRoutes({
-      //   airportsConnections,
-      //   numberOfFlights: currentSegment.numberOfFlights,
-      //   startAirport: currentSegment.departure,
-      //   endAirport: currentSegment.arrival,
-      // });
+      let allPossibleRoutesForThisSegment: string[] =
+        this.generatePossibleRoutesFromAirport({
+          departureICAO: currentSegment.departure,
+          airportConnectionData: airportsConnections[currentSegment.departure],
+          previousRoute: '',
+        });
 
-      const possibleRoutes = [[]];
-      const possibleRoutesV2 = this.findPossibleRoutesV2({
-        airportsConnections,
-        filterCriteria: {
-          filterDestinations: currentSegment.arrival
-            ? [currentSegment.arrival]
-            : undefined,
-          airportsRoutes: currentSegment.departure
-            ? [currentSegment.departure]
-            : undefined,
-        },
-      });
+      for (let i = 1; i < currentSegment.numberOfFlights; i++) {
+        const isLastFlightAndSegment =
+          !nextSegment && i == currentSegment.numberOfFlights - 1;
 
-      console.log('Quantidade de rotas possiveis', possibleRoutesV2.length);
+        const newPossibleRoutesForThisSegment: string[] = [];
+        allPossibleRoutesForThisSegment.forEach((currentFlightDuty) => {
+          const departureForNextLeg = last(currentFlightDuty.split('-')).trim();
 
-      if (!possibleRoutes || possibleRoutes.length == 0) {
-        console.log('Não achou rota possivel');
-        this.resetFinalRoute({ HUB });
-        return addRoutes({ index: 0 });
+          const possibleNextRoutes = this.generatePossibleRoutesFromAirport({
+            departureICAO: departureForNextLeg,
+            airportConnectionData: airportsConnections[departureForNextLeg],
+            previousRoute: currentFlightDuty,
+            filterCriteria: {
+              onlyDestinations: isLastFlightAndSegment ? [HUB] : undefined,
+            },
+          });
+
+          newPossibleRoutesForThisSegment.push(...possibleNextRoutes);
+        });
+
+        allPossibleRoutesForThisSegment = newPossibleRoutesForThisSegment;
       }
 
-      const route = sample(possibleRoutes);
-      this.finalRoute.push(...route);
-      currentSegment.arrival = route[route.length - 1];
-      currentSegment.route = route.toString();
-      if (nextSegment) {
-        console.log('Proximo segmento', nextSegment);
-        nextSegment.departure = route[route.length - 1];
-        addRoutes({ index: index + 1 });
+      if (allPossibleRoutesForThisSegment.length) {
+        currentSegment.route = sample(allPossibleRoutesForThisSegment);
+        currentSegment.arrival = last(currentSegment.route.split('-')).trim();
+        if (nextSegment) {
+          nextSegment.departure = currentSegment.arrival;
+          addRoutes({ segmentIndex: segmentIndex + 1 });
+        }
       }
     };
 
-    addRoutes({ index: 0 });
+    addRoutes({ segmentIndex: 0 });
   }
 }
