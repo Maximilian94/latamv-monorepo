f8ea988124733e2e11b3c05d91a991b963fb4463
Maximilian Kaden <mkadenfilgo@gmail.com>
2024-05-12 18:38:41 -0300

    tailwind css

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "@fontsource-variable/catamaran";
+import GlobalCssPriority from "./theme/GlobalCssPriority.tsx";
+import { ThemeProvider, createTheme } from "@mui/material";
+
+const theme = createTheme({});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <GlobalCssPriority>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </GlobalCssPriority>
   </React.StrictMode>
 );
