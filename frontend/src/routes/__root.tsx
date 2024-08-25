e9839593595dab175045a5b56f36f3670e29c12e
Maximilian Kaden <mkadenfilgo@gmail.com>
2024-08-25 21:52:26 +0200

    users-online-side-bar

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -2,23 +2,18 @@ import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import Navbar from "../components/navbar";
 import { Footer } from "../components/footer";
+import { SideBar } from "../components/sideBar";
 
 export const Route = createRootRoute({
   component: () => {
     return (
       <div className="h-screen bg-slate-50 flex flex-col justify-between">
         <Navbar />
-        {/* <div className="p-2 flex gap-2">
-          <Link to="/" className="[&.active]:font-bold">
-            Home
-          </Link>{" "}
-          <Link to="/about" className="[&.active]:font-bold">
-            About
-          </Link>
-        </div> */}
-        {/* <hr /> */}
-        <div className="h-full">
-          <Outlet />
+        <div className="flex w-full h-full">
+          <SideBar />
+          <div className="h-full">
+            <Outlet />
+          </div>
         </div>
         <Footer />
         <TanStackRouterDevtools />
