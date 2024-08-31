2b579d93fc359feab6ed400eeed151eac6307fdc
Maximilian Kaden <mkadenfilgo@gmail.com>
2024-08-31 08:29:00 +0200

    Implement logout

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,8 +1,24 @@
-import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
+import React, { useContext, useState } from "react";
+import { Disclosure } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Link, useMatchRoute } from "@tanstack/react-router";
 import { ToSubOptions } from "@tanstack/react-router";
+import {
+  Divider,
+  IconButton,
+  ListItemIcon,
+  Menu,
+  MenuItem,
+  Tooltip,
+  Box,
+  Typography,
+} from "@mui/material";
+import Avatar from "@mui/material/Avatar";
+import Settings from "@mui/icons-material/Settings";
+import { Logout } from "@mui/icons-material";
+import { AuthContext } from "../context/auth.context.tsx";
+import QueryBuilderIcon from "@mui/icons-material/QueryBuilder";
+import FlightTakeoffIcon from "@mui/icons-material/FlightTakeoff";
 
 interface NavigationOption {
   name: string;
@@ -21,6 +37,18 @@ function classNames(...classes: Array<string>) {
 }
 
 export default function Navbar() {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const openAnchor = Boolean(anchorEl);
+  const authContext = useContext(AuthContext);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   const matchRoute = useMatchRoute();
   return (
     <Disclosure as="nav" className="bg-indigo-950">
@@ -44,7 +72,7 @@ export default function Navbar() {
                 <div className="flex flex-shrink-0 items-center">
                   <img
                     className="h-8 w-auto"
-                    src="./latam-logo.svg"
+                    src="/latam-logo.svg"
                     alt="Your Company"
                   />
                 </div>
@@ -61,6 +89,7 @@ export default function Navbar() {
                           "rounded-md px-3 py-2 text-sm font-medium",
                         )}
                         aria-current={item.current ? "page" : undefined}
+                        search={""}
                       >
                         {item.name}
                       </Link>
@@ -79,69 +108,112 @@ export default function Navbar() {
                 </button>
 
                 {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-3">
-                  <div>
-                    <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="absolute -inset-1.5" />
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
+                <Tooltip title="Account settings">
+                  <IconButton
+                    onClick={handleClick}
+                    sx={{
+                      ml: 2,
+                      "&:focus": {
+                        outline: "none",
+                        boxShadow: "none",
+                      },
+                    }}
                   >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700",
-                            )}
+                    <Avatar
+                      variant="rounded"
+                      alt="Remy Sharp"
+                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  anchorEl={anchorEl}
+                  onClose={handleClose}
+                  open={openAnchor}
+                  transformOrigin={{ horizontal: "right", vertical: "top" }}
+                  anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+                >
+                  <Box sx={{ px: 2, py: 1 }}>
+                    <div
+                      style={{ display: "flex", gap: "10px", width: "100%" }}
+                    >
+                      <div style={{ width: "40px", height: "40px" }}>
+                        <img
+                          alt={"patent"}
+                          src={
+                            "https://cdn-icons-png.flaticon.com/512/3416/3416563.png"
+                          }
+                          style={{
+                            objectFit: "contain",
+                            height: "100%",
+                            width: "100%",
+                          }}
+                        />
+                      </div>
+                      <div>
+                        <div
+                          style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "flex-start",
+                          }}
+                        >
+                          <Typography variant={"caption"} lineHeight={1}>
+                            Co-Pilot
+                          </Typography>
+                          <Typography variant={"button"} lineHeight={1}>
+                            {authContext?.user?.name}
+                          </Typography>
+                        </div>
+                        <div
+                          style={{
+                            width: "100%",
+                            display: "flex",
+                            justifyContent: "space-between",
+                            gap: "8px",
+                          }}
+                        >
+                          <div
+                            style={{
+                              display: "flex",
+                              gap: "2px",
+                              alignItems: "center",
+                            }}
                           >
-                            Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700",
-                            )}
+                            <QueryBuilderIcon
+                              sx={{ fontSize: 14 }}
+                            ></QueryBuilderIcon>
+                            <Typography variant={"caption"}>200h</Typography>
+                          </div>
+                          <div
+                            style={{
+                              display: "flex",
+                              gap: "2px",
+                              alignItems: "center",
+                            }}
                           >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700",
-                            )}
-                          >
-                            Sign out
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
+                            <FlightTakeoffIcon
+                              sx={{ fontSize: 14 }}
+                            ></FlightTakeoffIcon>
+                            <Typography variant={"caption"}>75</Typography>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </Box>
+                  <Divider />
+                  <MenuItem onClick={handleClose}>
+                    <ListItemIcon>
+                      <Settings fontSize="small" />
+                    </ListItemIcon>
+                    Settings
+                  </MenuItem>
+                  <MenuItem onClick={() => authContext?.logout()}>
+                    <ListItemIcon>
+                      <Logout fontSize="small" />
+                    </ListItemIcon>
+                    Logout
+                  </MenuItem>
                 </Menu>
               </div>
             </div>
--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -1,7 +1,20 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
-import { Button, Card, TextField } from "@mui/material";
+import {
+  Card,
+  FormControl,
+  FormHelperText,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+} from "@mui/material";
 import { useAuth } from "../context/auth.context.tsx";
 import { Controller, useForm } from "react-hook-form";
+import LoadingButton from "@mui/lab/LoadingButton";
+import SendIcon from "@mui/icons-material/Send";
+import { useState } from "react";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 type FormData = {
   emailOrUsername: string;
@@ -10,7 +23,13 @@ type FormData = {
 
 function Login() {
   const auth = useAuth();
-  const { control, handleSubmit } = useForm<FormData>({
+  const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
     defaultValues: {
       emailOrUsername: "",
       password: "",
@@ -18,31 +37,94 @@ function Login() {
   });
 
   const onSubmit = handleSubmit(async (data) => {
+    console.log("On submit");
     try {
+      setLoading(true);
       await auth.login(data);
     } catch (error) {
       /* empty */
+    } finally {
+      setLoading(false);
     }
   });
 
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
   return (
-    <div className="min-h-full flex justify-center items-center bg-gray-900">
+    <div className="min-h-full flex flex-col gap-12 justify-center items-center bg-gray-900">
+      <img alt={"logo"} src={"/latam-logo.svg"} />
       <Card variant="outlined" className="p-4 shadow">
         <form onSubmit={onSubmit}>
           <div className="flex flex-col gap-2">
             <Controller
               name="emailOrUsername"
               control={control}
-              render={({ field }) => <TextField label="username" {...field} />}
+              rules={{
+                required: "Username or email is required",
+              }}
+              render={({ field }) => (
+                <TextField
+                  label="Username or E-mail"
+                  {...field}
+                  error={!!errors[field.name]}
+                  helperText={
+                    errors[field.name] ? errors[field.name]?.message : " "
+                  }
+                  className={errors[field.name] ? "animate-shake" : ""}
+                />
+              )}
             />
             <Controller
               name="password"
               control={control}
-              render={({ field }) => <TextField label="password" {...field} />}
+              rules={{
+                required: "Password is required",
+              }}
+              render={({ field }) => (
+                <FormControl
+                  className={errors[field.name] ? "animate-shake" : ""}
+                  error={!!errors[field.name]}
+                >
+                  <InputLabel htmlFor={`id-${field.name}`}>Password</InputLabel>
+                  <OutlinedInput
+                    id={`id-${field.name}`}
+                    label={"Password"}
+                    {...field}
+                    type={showPassword ? "text" : "password"}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          edge="end"
+                          sx={{
+                            outline: "none",
+                            "&:focus": {
+                              outline: "none",
+                              boxShadow: "none",
+                            },
+                          }}
+                        >
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                  ></OutlinedInput>
+                  <FormHelperText id={`id-${field.name}-helper-text`}>
+                    {errors[field.name] ? errors[field.name]?.message : " "}
+                  </FormHelperText>
+                </FormControl>
+              )}
             />
-            <Button type={"submit"} variant="contained">
+            <LoadingButton
+              type={"submit"}
+              variant="contained"
+              loadingPosition="end"
+              loading={loading}
+              endIcon={<SendIcon />}
+            >
               Login
-            </Button>
+            </LoadingButton>
           </div>
         </form>
       </Card>
