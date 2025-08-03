377cc51244a8fea52f18cf4674decf5e1d231a1d
Maximilian Kaden <mkadenfilgo@gmail.com>
2025-08-03 11:16:10 +0200

    fix: aircraft model required (#16)

--- backend/prisma/migrations/20250803090230_make_aircraft_model_required/migration.sql
+++ backend/prisma/migrations/20250803090230_make_aircraft_model_required/migration.sql
@@ -4,9 +4,6 @@
   - Made the column `aircraftModel` on table `Flight` required. This step will fail if there are existing NULL values in that column.
 
 */
--- AlterTable
-ALTER TABLE "Flight" ALTER COLUMN "aircraftModel" SET NOT NULL;
-
 -- Atualiza os dados existentes
 UPDATE "Flight" SET "aircraftModel" = 'Unknown' WHERE "aircraftModel" IS NULL;
 
