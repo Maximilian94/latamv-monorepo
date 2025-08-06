0a64392e34f776cc376fe2a5da9b7f33d647d7e6
Maximilian Kaden <mkadenfilgo@gmail.com>
2025-08-06 14:51:23 +0200

    fix: remove-eet-and-rename-eet_seconds

--- backend/prisma/migrations/20250806120708_drop_eet_rename_eet_seconds/migration.sql
+++ backend/prisma/migrations/20250806120708_drop_eet_rename_eet_seconds/migration.sql
@@ -1,11 +1,12 @@
 /*
   Warnings:
 
-  - You are about to drop the column `eet_seconds` on the `Route` table. All the data in the column will be lost.
-  - Changed the type of `eet` on the `Route` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+  - You are about to drop the column `eet` on the `Route` table. All the data in the column will be lost.
+  - You are about to rename the column `eet_seconds` on the `Route` table to `eet`.
 
 */
 -- AlterTable
-ALTER TABLE "Route" DROP COLUMN "eet_seconds",
-DROP COLUMN "eet",
-ADD COLUMN     "eet" INTEGER NOT NULL;
+ALTER TABLE "Route" DROP COLUMN "eet";
+
+-- Rename column
+ALTER TABLE "Route" RENAME COLUMN "eet_seconds" TO "eet";
--- backend/prisma/migrations/20250806122223_update_flight_eet_to_int/migration.sql
+++ backend/prisma/migrations/20250806122223_update_flight_eet_to_int/migration.sql
@@ -1,11 +1,12 @@
 /*
   Warnings:
 
-  - You are about to drop the column `eet_seconds` on the `Flight` table. All the data in the column will be lost.
-  - Changed the type of `eet` on the `Flight` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+  - You are about to drop the column `eet` on the `Flight` table. All the data in the column will be lost.
+  - You are about to rename the column `eet_seconds` on the `Flight` table to `eet`.
 
 */
 -- AlterTable
-ALTER TABLE "Flight" DROP COLUMN "eet_seconds",
-DROP COLUMN "eet",
-ADD COLUMN     "eet" INTEGER NOT NULL;
+ALTER TABLE "Flight" DROP COLUMN "eet";
+
+-- Rename column
+ALTER TABLE "Flight" RENAME COLUMN "eet_seconds" TO "eet";
